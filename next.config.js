bb14513ea8f76830a505643650184919754c9386
Hassan Shahid <43190902+habibshahid2013@users.noreply.github.com>
2023-03-26 16:59:29 -0500

    changed dependecies

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  outputDirectory: 'build'
+  outputDirectory: '.next',
+  distDir: 'build'
+
 }
 
 module.exports = nextConfig
