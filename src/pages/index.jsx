10d59d758a5ffec68ff0123ba2a73c9abed51263
Hassan Shahid <43190902+habibshahid2013@users.noreply.github.com>
2023-03-26 11:38:47 -0500

    editing to the index.jsx file and adding css also created test app.js

---
+++ src/pages/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.module.css';
+import App from './_app';
+import reportWebVitals from './reportWebVitals';
+
+export default function Home() {
+  return (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+  reportWebVitals();
+}
+
+//if (typeof document !== 'undefined') {
+//  const root = ReactDOM.createRoot(document.getElementById('root'));
+//  root.render(<Home />);
+//
