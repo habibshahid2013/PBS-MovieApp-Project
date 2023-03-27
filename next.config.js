270a55ad8dda2d6697433774a28a9dbea991da34
Hassan Shahid <43190902+habibshahid2013@users.noreply.github.com>
2023-03-27 18:16:54 -0500

    changed dependecies

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: 'build',
-  publicRuntimeConfig: {
-    basePath: process.env.NODE_ENV === 'production' ? '/final-pbs-movie-app' : '',
-  },
-};
-
-module.exports = nextConfig;
+}
+module.exports = nextConfig
